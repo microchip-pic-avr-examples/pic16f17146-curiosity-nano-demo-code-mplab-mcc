34fac7063fd0f85a1457ca36367630061d3503cc
Irene Mary Abraham <Irene.Abraham@microchip.com>
2022-06-10 02:43:32 -0600

    Pull request #4: Develop
    Merge in EBE/pic16f17146-curiosity-nano-demo-code-mplab-mcc from develop to master

---
+++ pic16f17146-curiosity-nano-demo-code-mplab-mcc.X/application.h
@@ -0,0 +1,47 @@
+/*
+© [2022] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef CNANO_APPLICATION_H
+#define	CNANO_APPLICATION_H
+
+#ifdef	__cplusplus
+extern "C"
+{
+#endif
+
+#define NO_OF_POINTS (128)   // Total number of points in one cycle of triangular wave 
+
+extern const uint8_t *LUT_ptr;
+extern const uint8_t triangleLUT[NO_OF_POINTS];
+
+void Application(void);
+void ChangeADCCMode(void);
+void UpdateDac(void);
+void TMR4_UserInterruptHandler(void);
+void TMR0_UserInterruptHandler(void);
+void ADCC_UserThresholdInterruptHandler(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* APPLICATION_H */
+
